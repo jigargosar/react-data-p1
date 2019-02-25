06e77d76041e494b10f48f3650e9cf4d5ade20a5
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-25 16:02:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="">
-        <h1>ReactDataP1</h1>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="">
+      <h1>ReactDataP1</h1>
+    </div>
+  )
 }
 
 export default App
