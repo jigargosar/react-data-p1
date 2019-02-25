db3f21f4bc68e9973ff999ad5097df99065da731
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-25 16:09:56 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
+import { observable } from 'mobx'
+import * as R from 'ramda'
+import faker from 'faker'
+import * as nanoid from 'nanoid'
+
+function createFakeRow() {
+  return { id: nanoid(), name: faker.name.findName() }
+}
+
+const store = observable.object({
+  rows: R.times(createFakeRow)(10),
+})
+
+const Row = observer(({ row }) => {
+  return <div className="ma3">{row.name}</div>
+})
+
+Row.displayName = 'Row'
+
+function renderRow(row) {
+  return <Row key={row.id} row={row} />
+}
 
 function App() {
   return (
     <div className="">
       <h1>ReactDataP1</h1>
+      {store.rows.map(renderRow)}
     </div>
   )
 }
