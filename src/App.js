5501e65f43d4c8001de99ae6d232b70541277de3
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-25 16:02:24 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div className="pa3">
+      <div className="">
         <h1>ReactDataP1</h1>
       </div>
     )
